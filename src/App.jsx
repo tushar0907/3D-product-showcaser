c3b3b114c9722753bd7e62cdeefe63ef2e130e74
Tushar Kumar <tusharkumar0807@gmail.com>
2023-04-10 12:37:39 +0530

    folder structure and home page minor setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 
 
 function App() {
 
   return (
-    <div>
-      Hello  
-    </div>
+    <main className='app transition-all ease-in'>
+      <Home />
+      <Canvas />
+      <Customizer />  
+    </main>
   )
 }
 
